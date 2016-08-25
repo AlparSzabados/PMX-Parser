5a1cc45d2fb5074e60b0a80e93e8c94a8b8a84f4
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-26 02:11:50 +0300

    Added more fields, corrected typos.

--- src/main/groovy/szabados/alpar/parseAll/Note.groovy
+++ src/main/groovy/szabados/alpar/parseAll/Note.groovy
@@ -4,25 +4,27 @@ import groovy.transform.Canonical
 
 @Canonical class Note extends StaffChild {
     int staffIndex
-    float horizontalOffset
-    float verticalOffset
-    int stem
+    float horizontalPosition
+    float verticalPosition
     Accidental accidental
-    float accidentalOffset
-    int accidParantheses
+    StemDirection stemDirection
+    float accidentalDisplacement
+    boolean accidentalInParentheses
     NoteHead noteHead
-    boolean noteHeadParantheses
+    boolean noteHeadInParentheses
     float duration
-    float stemLenght
-    float flag
-    float dot
+    float stemLength
+    int numberOfFlags
+    int numberOfDots
+    int dotsOffset
+    float dotDisplacement
     float leftRightDisplacement
     float marks
     int staffDisplacement
     float marksHorizontalOffset
     float marksVerticalOffset
     float size
-    float ledgerLineThicknes
-    float stemThicknes
+    float ledgerLineThickness
+    float stemThickness
     float markSize
 }
