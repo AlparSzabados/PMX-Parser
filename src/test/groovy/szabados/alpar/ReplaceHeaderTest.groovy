facc70914988a1a7877129d7e65809fec1e44dfa
AlparSzabados <szabadosalpar@gmail.com>
2016-08-25 17:16:16 +0300

    float numbers now have a leading zero.

--- src/test/groovy/szabados/alpar/ReplaceHeaderTest.groovy
+++ src/test/groovy/szabados/alpar/ReplaceHeaderTest.groovy
@@ -21,7 +21,7 @@ class ReplaceHeaderTest extends Specification {
 
         where: text                                                                                                  || result
                '0001.000 0002.3000 0008.2314'                                                                        || [['1.000', '2.3000', '8.2314']]
-               ' t 0001.0000 0000.0000 -0013.0000 0001.0000 0000.7100\n_00?c 2016 Schott Music GmbH & Co. KG, Mainz' || [['t', '1.0000', '.0000', '-13.0000', '1.0000', '.7100'], ['_00?c 2016 Schott Music GmbH & Co. KG, Mainz']]
+               ' t 0001.0000 0000.0000 -0013.0000 0001.0000 0000.7100\n_00?c 2016 Schott Music GmbH & Co. KG, Mainz' || [['t', '1.0000', '.0000', '-13.0000', '1.0000', '0.7100'], ['_00?c 2016 Schott Music GmbH & Co. KG, Mainz']]
     }
     /*@formatter:on*/
 }
