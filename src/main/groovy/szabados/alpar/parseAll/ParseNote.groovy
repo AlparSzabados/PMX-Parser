77678832caf5a068dde1bfba6e9caaba6abf58d7
AlparSzabados <szabadosalpar@gmail.com>
2016-08-25 17:14:00 +0300

    added value to noteHeadParantheses.

--- src/main/groovy/szabados/alpar/parseAll/ParseNote.groovy
+++ src/main/groovy/szabados/alpar/parseAll/ParseNote.groovy
@@ -2,13 +2,14 @@ package szabados.alpar.parseAll
 
 import static szabados.alpar.parseAll.Accidental.getAccidental
 import static szabados.alpar.parseAll.NoteHead.*
+import static Condition.*
 
 class ParseNote {
     static parseNote(List<String> value) {
         /*@formatter:off*/
         def value4 = value[4].toDouble()
         def value5 = value[5].toInteger()
-        return new Note(staffIndex:         value[1].toInteger(),
+        return new Note(staffIndex:         value[1].toFloat(),
                         horizontalOffset:   value[2].toFloat(),
                         verticalOffset:     value[3].toFloat(),
                         stem:               stripStem(value4),
@@ -16,7 +17,8 @@ class ParseNote {
                         accidentalOffset:   stripAccidOffset(value4),
                         accidParantheses:   (value4 >= 100) ? 100 : 0,
                         noteHead:           getNoteHead((value5 >= 10) ? (value5 - 10) : value5),
-                        size:               value[5].toFloat())
+                        noteHeadParantheses: value5 >= 10 ? YES : NO)
+//                        size:               value[].toFloat())
         /*@formatter:on*/
     }
 
