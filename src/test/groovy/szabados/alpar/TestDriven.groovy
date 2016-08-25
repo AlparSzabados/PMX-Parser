e507a990acccb0412ac6f9134c23f9aa815c4017
AlparSzabados <szabadosalpar@gmail.com>
2016-08-25 16:30:50 +0300

    More tests!

--- src/test/groovy/szabados/alpar/TestDriven.groovy
+++ src/test/groovy/szabados/alpar/TestDriven.groovy
@@ -4,6 +4,7 @@ import spock.lang.Specification
 import szabados.alpar.parseAll.Note
 import szabados.alpar.parseAll.Sheet
 import szabados.alpar.parseAll.Staff
+import szabados.alpar.parseAll.StaffChild
 import szabados.alpar.parseAll.Text
 
 import static szabados.alpar.Parser.parser
@@ -21,11 +22,22 @@ class TestDriven extends Specification {
 
         where:
         fileContent                                                      || result
-        '8 2 0 200\n8 2 0 150\n1 2 0 -13 1 0.71'                         || new Sheet(staffs: [new Staff(staffIndex: 2, horizontalOffset: 0, rightEnd: 200, elements: [new Staff(staffIndex: 2, horizontalOffset: 0, rightEnd: 150), new Note(staffIndex: 2, horizontalOffset: 0.0, verticalOffset: -13.0, accidental: FLAT, size: 0.71)])])
-        't 1 0 -13 1 0.71\n_00?c 2016 Schott Music GmbH & Co. KG, Mainz\n'+
-        't 1 0 -13 1 0.71\n_00?c 2016 Schott Music GmbH & Co. KG, Mainz\n'+
-        '1 2 0 -13 1 0.71\n8 2 0 200'                                    || new Sheet(staffs: [new Staff(staffIndex: 1, elements: [new Text(staffIndex: 1, horizontalOffset: 0.0, verticalOffset: -13.0, kerning: 1.0, fontSize: 0.71, text: '_00?c 2016 Schott Music GmbH & Co. KG, Mainz'), new Text(staffIndex: 1, horizontalOffset: 0.0, verticalOffset: -13.0, kerning: 1.0, fontSize: 0.71, text: '_00?c 2016 Schott Music GmbH & Co. KG, Mainz')]), new Staff(staffIndex: 2, horizontalOffset: 0.0, rightEnd: 200.0, elements: [new Note(staffIndex: 2, horizontalOffset: 0.0, verticalOffset: -13.0, accidental: FLAT, size: 0.71)])])
-        '1 2 0 -13 1 0.71\n8 2 0 200'                                    || new Sheet(staffs: [new Staff(staffIndex: 2, horizontalOffset: 0.0, rightEnd: 200.0, elements: [new Note(staffIndex: 2, horizontalOffset: 0.0, verticalOffset: -13.0, accidental: FLAT, size: 0.71)])])
+        '1 2 0 -13 111.99 0.71\n'+
+        '8 2 0 200'                                                      || new Sheet(staffs: [new Staff(staffIndex: 2, horizontalOffset: 0.0, rightEnd: 200.0, elements: [new Note(staffIndex: 2, horizontalOffset: 0.0, verticalOffset: -13.0, stem: 10, accidental: FLAT, accidentalOffset: 0.99, accidParantheses: 100, size: 0.71)])])
+
+        '8 2 0 200\n'+
+        '8 2 0 150\n'+
+        '1 2 0 -13 1 0.71'                                               || new Sheet(staffs: [new Staff(staffIndex: 2, horizontalOffset: 0, rightEnd: 200, elements: [new Staff(staffIndex: 2, horizontalOffset: 0, rightEnd: 150),
+                                                                                                                                                                       new Note(staffIndex: 2, horizontalOffset: 0.0, verticalOffset: -13.0, accidental: FLAT, size: 0.71)])])
+
+        't 1 0 -13 1 0.71\n'+
+        '_00?c 2016 Schott Music GmbH & Co. KG, Mainz\n'+
+        't 1 0 -13 1 0.71\n'+
+        '_00?c 2016 Schott Music GmbH & Co. KG, Mainz\n'+
+        '1 2 0 -13 1 0.71\n'+
+        '8 2 0 200'                                                      || new Sheet(staffs: [new Staff(staffIndex: 1, elements: [new Text(staffIndex: 1, horizontalOffset: 0.0, verticalOffset: -13.0, kerning: 1.0, fontSize: 0.71, text: '_00?c 2016 Schott Music GmbH & Co. KG, Mainz'),
+                                                                                                                                   new Text(staffIndex: 1, horizontalOffset: 0.0, verticalOffset: -13.0, kerning: 1.0, fontSize: 0.71, text: '_00?c 2016 Schott Music GmbH & Co. KG, Mainz')]),
+                                                                                               new Staff(staffIndex: 2, horizontalOffset: 0.0, rightEnd: 200.0, elements: [new Note(staffIndex: 2, horizontalOffset: 0.0, verticalOffset: -13.0, accidental: FLAT, size: 0.71)])])
     }
     /*@formatter:on*/
 }
