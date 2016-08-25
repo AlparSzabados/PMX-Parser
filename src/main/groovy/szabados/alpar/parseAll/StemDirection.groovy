41886338c9adedf06a3cb6e211c47217cdedc8f6
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-26 02:17:56 +0300

    Stem directions are now enum values and not numbers. Makes the code more readable.

---
+++ src/main/groovy/szabados/alpar/parseAll/StemDirection.groovy
@@ -0,0 +1,22 @@
+package szabados.alpar.parseAll
+
+enum StemDirection {
+    NO_STEM(0),
+    UP(10),
+    DOWN(20)
+
+    private final int ordinal
+
+    private StemDirection(int ordinal) {
+        this.ordinal = ordinal
+    }
+
+    public static StemDirection getStemDirection(int ordinal) {
+        for (StemDirection stems : values()) {
+            if (stems.ordinal == ordinal)
+                return stems
+        }
+        throw new IllegalArgumentException()
+    }
+}
+
