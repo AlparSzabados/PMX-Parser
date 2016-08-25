d67050c2552628fe909fbed3b00d5a78bf61bad6
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-26 02:15:26 +0300

    StaffIndex and HorizontalIndex are now cast as Float numbers.

--- src/main/groovy/szabados/alpar/parseAll/ParseStaff.groovy
+++ src/main/groovy/szabados/alpar/parseAll/ParseStaff.groovy
@@ -3,8 +3,8 @@ package szabados.alpar.parseAll
 class ParseStaff {
     static parseStaff(List<String> value) {
         /*@formatter:off*/
-        return new Staff(staffIndex:       value[1].toInteger(),
-                         horizontalOffset: value[2].toInteger(),
+        return new Staff(staffIndex:       value[1].toFloat(),
+                         horizontalOffset: value[2].toFloat(),
                          rightEnd:         value[3].toFloat())
         /*@formatter:on*/
     }
--- src/main/groovy/szabados/alpar/parseAll/ParseText.groovy
+++ src/main/groovy/szabados/alpar/parseAll/ParseText.groovy
@@ -3,7 +3,7 @@ package szabados.alpar.parseAll
 class ParseText {
     static parseText(List<String> value, List<String> text) {
         /*@formatter:off*/
-        return new Text(staffIndex:       value[1].toInteger(),
+        return new Text(staffIndex:       value[1].toFloat(),
                         horizontalOffset: value[2].toFloat(),
                         verticalOffset:   value[3].toFloat(),
                         kerning:          value[4].toFloat(),
