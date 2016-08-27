691f457c5d2b9046d92c1c2a84e959ca9e3d8c69
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-28 01:43:24 +0300

    Simplified the code by removing the FirstValue list.

--- src/main/groovy/szabados/alpar/parseAll/Elements.groovy
+++ src/main/groovy/szabados/alpar/parseAll/Elements.groovy
@@ -26,26 +26,8 @@ enum Elements {
         this.ordinal = ordinal
     }
 
-    private static final List<String> FirstValue = ['1.0000',
-                                                    '2.0000',
-                                                    '3.0000',
-                                                    '4.0000',
-                                                    '5.0000',
-                                                    '6.0000',
-                                                    '7.0000',
-                                                    '8.0000',
-                                                    '9.0000',
-                                                    '10.0000',
-                                                    '11.0000',
-                                                    '12.0000',
-                                                    '13.0000',
-                                                    '14.0000',
-                                                    '15.0000',
-                                                    't',
-                                                    '17.0000',
-                                                    '18.0000']
-
     public static getElement(Elements types) {
-        return FirstValue[types.ordinal - 1]
+        if (types.ordinal == 16) return 't'
+        "${types.ordinal}.0000"
     }
 }
