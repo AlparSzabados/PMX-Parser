d52f750ba8224a4b0edbe491a2574948568a89b0
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-28 01:33:32 +0300

    Updated test for Marks.

--- src/test/groovy/szabados/alpar/NoteTest.groovy
+++ src/test/groovy/szabados/alpar/NoteTest.groovy
@@ -8,6 +8,7 @@ import szabados.alpar.parseAll.Staff
 import static szabados.alpar.Parser.parser
 import static szabados.alpar.ReplaceHeader.replaceHeader
 import static szabados.alpar.parseAll.Accidental.FLAT
+import static szabados.alpar.parseAll.Marks.STACCATO
 import static szabados.alpar.parseAll.NoteHead.BLACK
 import static szabados.alpar.parseAll.StemDirection.UP
 
@@ -23,7 +24,7 @@ class NoteTest extends Specification {
         fileContent = '0008.0000 0002.0000 0.0000 0200.0000\n'+
                 '0001.0000 0002.0000 0000.0000 -0013.0000 0111.9900 0001.0000 0000.5000 0001.0000 1122.9300 0002.3000 0007.5000 0000.0000 -0001.0000 0002.0000 0000.0000 0000.0000 0002.0000 0001.2000'
         result = new Sheet(staffs: [new Staff(staffIndex: 2, horizontalOffset: 0.0, rightEnd: 200.0, elements:
-                [new Note(staffIndex: 2, horizontalPosition: 0.0, verticalPosition: -13.0, stemDirection: UP, accidental: FLAT, accidentalDisplacement: 0.99, accidentalInParentheses: true, noteHead: BLACK, noteHeadInParentheses: false, duration: 0.5, stemLength: 1, numberOfFlags: 2, numberOfDots: 20, dotsOffset: 1100, dotDisplacement: 0.93, leftRightDisplacement: 2.3, marks: 7.5, staffDisplacement: 0, marksHorizontalOffset: -1, marksVerticalOffset: 2, size: 0, ledgerLineThickness: 0, stemThickness: 2, markSize: 1.2)])])
+                [new Note(staffIndex: 2, horizontalPosition: 0.0, verticalPosition: -13.0, stemDirection: UP, accidental: FLAT, accidentalDisplacement: 0.99, accidentalInParentheses: true, noteHead: BLACK, noteHeadInParentheses: false, duration: 0.5, stemLength: 1, numberOfFlags: 2, numberOfDots: 20, dotsOffset: 1100, dotDisplacement: 0.93, leftRightDisplacement: 2.3, marks: STACCATO, marksDisplacement: 0.5, staffDisplacement: 0, marksHorizontalOffset: -1, marksVerticalOffset: 2, size: 0, ledgerLineThickness: 0, stemThickness: 2, markSize: 1.2)])])
     }
     /*@formatter:on*/
 }
--- src/test/groovy/szabados/alpar/TestDriven.groovy
+++ src/test/groovy/szabados/alpar/TestDriven.groovy
@@ -1,6 +1,7 @@
 package szabados.alpar
 
 import spock.lang.Specification
+import szabados.alpar.parseAll.Marks
 import szabados.alpar.parseAll.Note
 import szabados.alpar.parseAll.Sheet
 import szabados.alpar.parseAll.Staff
@@ -9,10 +10,10 @@ import szabados.alpar.parseAll.Text
 import static szabados.alpar.Parser.parser
 import static szabados.alpar.ReplaceHeader.replaceHeader
 import static szabados.alpar.parseAll.Accidental.FLAT
+import static szabados.alpar.parseAll.Marks.*
 import static szabados.alpar.parseAll.NoteHead.BLACK
 import static szabados.alpar.parseAll.StemDirection.NO_STEM
 
-//TODO make it work for the other test too
 class TestDriven extends Specification {
     /*@formatter:off*/
     def 'test driven development'() {
@@ -26,7 +27,7 @@ class TestDriven extends Specification {
         '0008.0000 0002.0000 0.0000 0200.0000\n'+
         '0008.0000 0002.0000 0.0000 0150.0000\n'+
         '0001.0000 0002.0000 0.0000 -0013.0000 1.0000 1.0000'                       || new Sheet(staffs: [new Staff(staffIndex: 2, horizontalOffset: 0, rightEnd: 200, elements: [new Staff(staffIndex: 2, horizontalOffset: 0, rightEnd: 150),
-                                                                                                                                                                                  new Note(staffIndex: 2, horizontalPosition: 0.0, verticalPosition: -13.0, accidental: FLAT, stemDirection: NO_STEM, noteHead: BLACK, noteHeadInParentheses: false)])])
+                                                                                                                                                                                  new Note(staffIndex: 2, horizontalPosition: 0.0, verticalPosition: -13.0, accidental: FLAT, stemDirection: NO_STEM, noteHead: BLACK, noteHeadInParentheses: false, marks: NONE)])])
 
         't 1.0000 0.0000 -0013.0000 0001.0000 0.7100\n'+
         '_00?c 2016 Schott Music GmbH & Co. KG, Mainz\n'+
@@ -35,7 +36,7 @@ class TestDriven extends Specification {
         '0001.0000 0002.0000 0.0000 -0013.0000 1.0000 1.0000\n'+
         '0008.0000 0002.0000 0.0000 0200.0000'                                      || new Sheet(staffs: [new Staff(staffIndex: 1, elements: [new Text(staffIndex: 1, horizontalOffset: 0.0, verticalOffset: -13.0, kerning: 1.0, fontSize: 0.71, text: '_00?c 2016 Schott Music GmbH & Co. KG, Mainz'),
                                                                                                                                    new Text(staffIndex: 1, horizontalOffset: 0.0, verticalOffset: -13.0, kerning: 1.0, fontSize: 0.71, text: '_00?c 2016 Schott Music GmbH & Co. KG, Mainz')]),
-                                                                                                                                   new Staff(staffIndex: 2, horizontalOffset: 0.0, rightEnd: 200.0, elements: [new Note(staffIndex: 2, horizontalPosition: 0.0, verticalPosition: -13.0, accidental: FLAT, stemDirection: NO_STEM, noteHead: BLACK, noteHeadInParentheses: false)])])
+                                                                                                                                   new Staff(staffIndex: 2, horizontalOffset: 0.0, rightEnd: 200.0, elements: [new Note(staffIndex: 2, horizontalPosition: 0.0, verticalPosition: -13.0, accidental: FLAT, stemDirection: NO_STEM, noteHead: BLACK, noteHeadInParentheses: false, marks: NONE)])])
     }
     /*@formatter:on*/
 }
