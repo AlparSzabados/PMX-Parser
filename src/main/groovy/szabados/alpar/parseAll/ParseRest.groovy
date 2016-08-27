c0d357cb458d040ed42f039ba394132382057def
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-28 01:32:31 +0300

    Added Rests!

---
+++ src/main/groovy/szabados/alpar/parseAll/ParseRest.groovy
@@ -0,0 +1,23 @@
+package szabados.alpar.parseAll
+
+class ParseRest {
+    static parseRest(List<String> value) {
+        /*@formatter:off*/
+        return new Rest(staffIndex:             value[1]?.toFloat() ?: 0,
+                        horizontalPosition:     value[2]?.toFloat() ?: 0,
+                        verticalPosition:       value[3]?.toFloat() ?: 0,
+                        restType:               value[4]?.toFloat() ?: 0 as int,
+                        dots:                   value[5]?.toFloat() ?: 0,
+                        duration:               value[6]?.toFloat() ?: 0,
+                        number:                 value[7]?.toFloat() ?: 0,
+                        centering:              value[8]?.toFloat() ?: 0,
+                        horizontalDisplacement: value[9]?.toFloat() ?: 0,
+                        numberVerticalOffset:   value[10]?.toFloat() ?: 0,
+                        numberSize:             value[11]?.toFloat() ?: 0,
+                        cues:                   value[12]?.toFloat() ?: 0,
+                        numberFont:             value[13]?.toFloat() ?: 0,
+                        size:                   value[14]?.toFloat() ?: 0,
+                        numberHorizontalOffset: value[15]?.toFloat() ?: 0)
+      /*@formatter:on*/
+    }
+}
