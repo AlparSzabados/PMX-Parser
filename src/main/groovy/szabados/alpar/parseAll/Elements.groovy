6227fc5a9ce9e3b70f92957c88c69c171923591a
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-28 00:31:31 +0300

    Added the rest of the other Elements.

--- src/main/groovy/szabados/alpar/parseAll/Elements.groovy
+++ src/main/groovy/szabados/alpar/parseAll/Elements.groovy
@@ -3,8 +3,22 @@ package szabados.alpar.parseAll
 enum Elements {
     NOTE(1),
     REST(2),
+    CLEF(3),
+    LINE(4),
+    SLUR(5),
+    BEAM(6),
+    TRILL(7),
+    STAFF(8),
+    MISC(9),
+    NUMBER(10),
+    DRAW(11),
+    CIRCLE(12),
+    DUMMY(13),
+    BAR_LINE(14),
+    CIEL(15),
     TEXT(16),
-    STAFF(8)
+    KEY_SIGNATURE(17),
+    METER(18)
 
     private final int ordinal
 
