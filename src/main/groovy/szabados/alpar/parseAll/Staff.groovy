2f8dd9b9e548770e698816509f5ff0e580e8f867
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-28 01:32:53 +0300

    `

--- src/main/groovy/szabados/alpar/parseAll/Staff.groovy
+++ src/main/groovy/szabados/alpar/parseAll/Staff.groovy
@@ -2,7 +2,8 @@ package szabados.alpar.parseAll
 
 import groovy.transform.Canonical
 
-@ Canonical class Staff extends StaffChild {
+@ Canonical
+class Staff extends StaffChild {
     int staffIndex
     float horizontalOffset
     float rightEnd
