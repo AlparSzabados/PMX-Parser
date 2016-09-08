61b33cd9d903f2e4c6448479e57f3276b62296bc
alpar <szabadosalpar@gmail.com>
2016-09-08 22:09:45 +0300

    Code formatting.

--- src/main/groovy/szabados/alpar/parseAll/Note.groovy
+++ src/main/groovy/szabados/alpar/parseAll/Note.groovy
@@ -4,29 +4,29 @@ import groovy.transform.Canonical
 
 @Canonical
 class Note extends StaffChild {
-    int staffIndex
-    float horizontalPosition
-    float verticalPosition
-    Accidental accidental
+    int           staffIndex
+    float         horizontalPosition
+    float         verticalPosition
+    Accidental    accidental
     StemDirection stemDirection
-    float accidentalDisplacement
-    boolean accidentalInParentheses
-    NoteHead noteHead
-    boolean noteHeadInParentheses
-    float duration
-    float stemLength
-    int numberOfFlags
-    int numberOfDots
-    int dotsOffset
-    float dotDisplacement
-    float leftRightDisplacement
-    Marks marks
-    float marksDisplacement
-    int staffDisplacement
-    float marksHorizontalOffset
-    float marksVerticalOffset
-    float size
-    float ledgerLineThickness
-    float stemThickness
-    float markSize
+    float         accidentalDisplacement
+    boolean       accidentalInParentheses
+    NoteHead      noteHead
+    boolean       noteHeadInParentheses
+    float         duration
+    float         stemLength
+    int           numberOfFlags
+    int           numberOfDots
+    int           dotsOffset
+    float         dotDisplacement
+    float         leftRightDisplacement
+    Marks         marks
+    float         marksDisplacement
+    int           staffDisplacement
+    float         marksHorizontalOffset
+    float         marksVerticalOffset
+    float         size
+    float         ledgerLineThickness
+    float         stemThickness
+    float         markSize
 }
