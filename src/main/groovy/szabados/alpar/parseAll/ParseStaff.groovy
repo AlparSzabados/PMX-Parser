dd6f2cf160ed6a29adb2c6d2200f24d5833ba39d
alpar <szabadosalpar@gmail.com>
2016-09-08 22:11:02 +0300

    Code formatting.

--- src/main/groovy/szabados/alpar/parseAll/ParseStaff.groovy
+++ src/main/groovy/szabados/alpar/parseAll/ParseStaff.groovy
@@ -2,11 +2,9 @@ package szabados.alpar.parseAll
 
 class ParseStaff {
     static parseStaff(List<String> value) {
-        /*@formatter:off*/
-        return new Staff(staffIndex:       value[1]?.toFloat() ?: 0,
-                         horizontalOffset: value[2]?.toFloat() ?: 0,
-                         rightEnd:         value[3]?.toFloat() ?: 0,
-                         size:             value[4]?.toFloat() ?: 0)
-        /*@formatter:on*/
+        return new Staff([staffIndex      : value[1]?.toFloat() ?: 0,
+                          horizontalOffset: value[2]?.toFloat() ?: 0,
+                          rightEnd        : value[3]?.toFloat() ?: 0,
+                          size            : value[4]?.toFloat() ?: 0])
     }
 }
