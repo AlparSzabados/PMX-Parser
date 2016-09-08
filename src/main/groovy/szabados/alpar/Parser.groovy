16e4644ed121f0f673adbc34e9c340d5810b45e7
AlparSzabados <szabadosalpar@gmail.com>
2016-09-08 13:52:12 +0300

    Renamed and extracted variables to make the code more understandable.

--- src/main/groovy/szabados/alpar/Parser.groovy
+++ src/main/groovy/szabados/alpar/Parser.groovy
@@ -1,13 +1,12 @@
 package szabados.alpar
 
-import szabados.alpar.parseAll.ParseRest
 import szabados.alpar.parseAll.Sheet
 import szabados.alpar.parseAll.Staff
 import szabados.alpar.parseAll.StaffChild
 
 import static szabados.alpar.parseAll.Elements.*
 import static szabados.alpar.parseAll.ParseNote.parseNote
-import static szabados.alpar.parseAll.ParseRest.*
+import static szabados.alpar.parseAll.ParseRest.parseRest
 import static szabados.alpar.parseAll.ParseStaff.parseStaff
 import static szabados.alpar.parseAll.ParseText.parseText
 
@@ -15,50 +14,52 @@ import static szabados.alpar.parseAll.ParseText.parseText
 class Parser {
     static Sheet parser(List<List<String>> list) {
         List<Staff> staffs = []
-        List<StaffChild> elements = []
+        List<StaffChild> staffElements = []
 
-        float currentStaff = 0.0
+        float currentStaffIndex = 0.0
         for (int i in 0..<list.size()) {
             List<String> currentElement = list[i]
             List<String> nextElement = list[i + 1]
+            String musicalElement = currentElement[0]
+            float musicalElementStaffIndex = currentElement[1].toFloat()
 
-            if (currentElement[0] == getElement(STAFF) && currentElement[1].toFloat() != currentStaff) {
+            if (musicalElement == elementType(STAFF) && musicalElementStaffIndex != currentStaffIndex) {
                 staffs += parseStaff(currentElement)
-                currentStaff = currentElement[1].toFloat()
-            } else if (currentElement[0] == getElement(STAFF) && currentElement[1].toFloat() == currentStaff)
-                elements += parseStaff(currentElement)
-            else if (currentElement[0] == getElement(NOTE))
-                elements += parseNote(currentElement)
-            else if (currentElement[0] == getElement(REST))
-                elements += parseRest(currentElement)
-            else if (currentElement[0] == getElement(TEXT))
-                elements += parseText(currentElement, nextElement)
-            else if (currentElement[0] == getElement(REST))
-                elements += parseRest(currentElement)
+                currentStaffIndex = musicalElementStaffIndex
+            } else if (musicalElement == elementType(STAFF) && musicalElementStaffIndex == currentStaffIndex)
+                staffElements += parseStaff(currentElement)
+            else if (musicalElement == elementType(NOTE))
+                staffElements += parseNote(currentElement)
+            else if (musicalElement == elementType(REST))
+                staffElements += parseRest(currentElement)
+            else if (musicalElement == elementType(TEXT))
+                staffElements += parseText(currentElement, nextElement)
+            else if (musicalElement == elementType(REST))
+                staffElements += parseRest(currentElement)
         }
 
         def staffsNeeded = 0
-        for (int i in 0..<elements.size()) {
+        for (int i in 0..<staffElements.size()) {
             if (staffs.size() == 0) {
-                staffs += new Staff(staffIndex: elements[i].staffIndex)
-                staffsNeeded = elements[i].staffIndex
+                staffs += new Staff(staffIndex: staffElements[i].staffIndex)
+                staffsNeeded = staffElements[i].staffIndex
             }
             for (int j in 0..<staffs.size()) {
-                if (elements[i].staffIndex == staffs[j].staffIndex && elements[i].staffIndex > staffsNeeded) {
-                    staffsNeeded = elements[i].staffIndex
-                } else if (elements[i].staffIndex > staffsNeeded) {
-                    staffs += new Staff(staffIndex: elements[i].staffIndex)
-                    staffsNeeded = elements[i].staffIndex
+                if (staffElements[i].staffIndex == staffs[j].staffIndex && staffElements[i].staffIndex > staffsNeeded) {
+                    staffsNeeded = staffElements[i].staffIndex
+                } else if (staffElements[i].staffIndex > staffsNeeded) {
+                    staffs += new Staff(staffIndex: staffElements[i].staffIndex)
+                    staffsNeeded = staffElements[i].staffIndex
                 }
             }
         }
 
         staffs.sort()
 
-        for (int i in 0..<elements.size())
+        for (int i in 0..<staffElements.size())
             for (int j in 0..<staffs.size())
-                if (elements[i].staffIndex == staffs[j].staffIndex)
-                    staffs[j].elements += elements[i]
+                if (staffElements[i].staffIndex == staffs[j].staffIndex)
+                    staffs[j].elements += staffElements[i]
 
         return new Sheet(staffs: staffs)
     }
